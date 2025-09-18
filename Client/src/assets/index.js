b2e3d0c2efd4976123bd22add68fae985cdc550a
Aditya <adityas.singh.tech@gmail.com>
2025-09-18 21:09:05 +0530

    feat: import images

--- Client/src/assets/index.js
+++ Client/src/assets/index.js
@@ -1,5 +1,7 @@
-import dashboard from './dashboard.svg'
+import dashboard from "./dashboard.svg";
+import feature from "./feature.svg";
+import AtsImg from "./atsImg.png"
+import CTA from './CTA.png'
+import signupbg from './signupbg.png'
 
-export {
-    dashboard,
-}
+export { dashboard, feature,AtsImg,CTA,signupbg };
