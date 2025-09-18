e2580d11eb8beac9f880e92b7225af9f83ca2e93
Aditya <adityas.singh.tech@gmail.com>
2025-09-18 21:17:14 +0530

    add component imports

--- Client/src/Components/index.js
+++ Client/src/Components/index.js
@@ -1,6 +1,13 @@
+import Logo from "./Logo";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Process from "./Process";
 import Features from "./Features";
+import Pricing from "./Pricing";
+import Testimonial from "./Testimonial";
+import TestimonialCard from "./TestimonialCard";
+import Footer from "./Footer";
+import FeatureCards from "./FeatureCards";
+import FileUpload from "./FileUpload";
 
-export { Navbar, Hero,Process,Features };
+export { Logo,Navbar, Hero,Process,Features,Pricing,Testimonial,TestimonialCard,Footer,FeatureCards,FileUpload };
